636545f30d0558f51c25d0f691f9ba725fa6b6a2
qufeng107 <56883973+qufeng107@users.noreply.github.com>
2020-01-30 11:44:54 +0000

    Update sql.java

--- Software_Code/AgileJavaHospitalProject/web/sql.java
+++ Software_Code/AgileJavaHospitalProject/web/sql.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package TreatmentFinder;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  *
  * @author fengqu
@@ -13,21 +14,23 @@ public class sql {
     
     public static String selectAll = "SELECT * FROM lol.operations";
     
-    public static String selectInIndex(String healthCare,int budget)
+    public static String selectInIndex(String search,String budget)
     {
-        if (healthCare.equals("Heart"))
-            return "SELECT * FROM lol.operations where DRG_Definition LIKE '001%' AND Average_Total_Payments < "+budget+" order by Average_Total_Payments desc";
-        
-        
-        else
-            return "";
+        Pattern pattern = Pattern.compile("[0-9]*"); 
+        if (!pattern.matcher(budget).matches())  //Search without input budget
+            if(pattern.matcher(search).matches())
+                if(Integer.parseInt(search)<1000)
+                    return "SELECT * FROM lol.operations where DRG_Definition LIKE '"+search+"%' order by Average_Total_Payments desc";
+                else
+                    return "SELECT * FROM lol.operations where Provider_Id = "+Integer.parseInt(search)+" order by Average_Total_Payments desc";
+            else
+                 return "call lol.findCode(\"" + search + "\")";
+        else    //Search with budget
+            return "SELECT * FROM lol.operations where DRG_Definition LIKE '%"+search+"%' AND Average_Total_Payments < "+Float.parseFloat(budget)+" order by Average_Total_Payments desc";
+
     }
     
     
-    public static String selectByCode(int code)
-    {
-        return "SELECT * FROM lol.operations where Provider_Id = "+code;
-    }
-    
+
     
 }
